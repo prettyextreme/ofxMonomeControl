dc299c6d7d843280e60e3802067d0d697c61c5d4
Josh Silverman <jhsilverman@gmail.com>
2012-04-11 12:01:58 -0300

    Update src/ofxMonomeControl.h

--- src/ofxMonomeControl.h
+++ src/ofxMonomeControl.h
@@ -1,15 +1,11 @@
 #ifndef ___MONOME_CONTROL__
 #define ___MONOME_CONTROL__
 
-
 // ofxMonomeControl.h
 // ofxMonomeControl.cpp
 //
 // Created by Josh Silverman on 3/28/2012.
 // prettyextreme.com
-// Creative Commons... Free for whatever the hell you want to do with it. 
-// Sell it. Whatever.
-// No warrantee... blah blah blah
 //
 // This is just a basic class to simplify direct serial communications
 // between OF and a monome. I just got tired of interfacing with monomeSerial.
@@ -18,17 +14,11 @@
 //
 // I tested this code with a monome128 under Windows 7.
 // It SHOULD PROBABLY work on other platforms with other monomes.
-// But I make no promises.
 //  
 // Tested under of 007
 //
 // Note: Not all features of the protocol have been included.
 // (Especially brightness and ADCs for the 40h)
-// But you've gotten this far. I'm sure you can figure it out!
-//  
-// Note: I admit, there is no reconnection functionality here.
-// Not the most "robust" thing in the world. But it works for me.
-
 
 #include "ofSerial.h"
 
